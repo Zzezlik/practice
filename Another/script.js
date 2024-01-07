1251eb4d42b5c97f816bf4e15066b47c0f07bcfd
Maksim M <118403951+Zzezlik@users.noreply.github.com>
2024-01-07 18:33:25 +0200

    .

---
+++ Another/script.js
@@ -0,0 +1,13 @@
+const msgList = document.querySelector('ul');
+
+const onCloseBtn = (e) => {
+    const { target } = e;
+
+    if (target.nodeName !== 'BUTTON' && target.tagName !== 'BUTTON') return
+
+    const msg = target.closest('li');
+    console.dir(msg);
+    msg.remove();
+}
+
+msgList.addEventListener('click', onCloseBtn)
