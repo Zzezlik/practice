5a0a17f7cedba7c66d98f419c089f67b4e1531ee
Maksim M <118403951+Zzezlik@users.noreply.github.com>
2024-01-14 08:39:56 +0200

    a

--- Another/script.js
+++
@@ -1,7 +0,0 @@
-const searchEl = document.querySelector("#searchInput");
-const itemList = document.querySelector("#itemList");
-const item = document.querySelector(".item");
-
-console.dir(itemList);
-
-searchEl.addEventListener("input", inputChange);
